2a9cdfa4d5af7841359a13e57ac18d1b15b20f1d
Aaron Housh <dispersias@gmail.com>
2021-11-28 16:56:03 -0700

    add rust commands

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -7,13 +7,23 @@ require('rust-tools').setup({
     hover_with_actions = true,
     inlay_hints = {
       show_parameter_hints = false,
-      parameter_hitns_prefix = "",
+      parameter_hints_prefix = "",
       other_hints_prefix = "",
     },
   },
   server = {
     capabilities = lsp_settings.Capabilities,
-    on_attach = lsp_settings.on_attach,
+    on_attach = function(client, bufnr)
+      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+      local opts = { noremap = true, silent = true }
+
+      buf_set_keymap('n', '<leader>ct', ':!cargo test<CR>', opts)
+      buf_set_keymap('n', '<leader>cb', ':!cargo build<CR>', opts)
+      buf_set_keymap('n', '<leader>cr', ':!cargo run<CR>', opts)
+
+      lsp_settings.on_attach(client, bufnr)
+    end,
     flags = {
       debounce_text_changes = 250,
     },
