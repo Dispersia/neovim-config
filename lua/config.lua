f35c3e0c907dd88ac4ed40f1a93ed5797ddffd4c
Aaron Housh <dispersias@gmail.com>
2021-12-12 12:20:16 -0700

    update neovim

--- lua/config.lua
+++ lua/config.lua
@@ -46,7 +46,7 @@ vim.api.nvim_exec(
   [[
   augroup AutoLSP
     autocmd!
-    autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({ focusable = false })
+    autocmd CursorHold * lua vim.diagnostic.open_float({ focusable = false })
   augroup end
   ]],
   false
